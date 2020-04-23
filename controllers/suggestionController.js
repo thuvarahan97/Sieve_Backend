e42ec9a70f4cdfcca0580847ae1df38e096b6d8e
Thenusan Santhirakumar <46936272+sthenusan@users.noreply.github.com>
2020-04-23 10:46:50 +0530

    Add files via upload

---
+++ controllers/suggestionController.js
@@ -0,0 +1,16 @@
+const Suggestion = require('../models/suggestionModel');
+
+exports.insert = (req, res, next) => {
+    const userId = req.body.userId;
+    const suggestion = req.body.suggestion;
+
+    if((userId)&&(suggestion)){
+        Suggestion.insert(req.body).then(()=>{
+            res.status(200).json({success: true});
+        }).catch(()=>{
+            res.status(404).json({serverError: true,error: 'Database Connection Faliure!' })
+        })
+    }else{
+        res.status(404).json({ serverError: false, error: 'Invalid Suggestion Format' });
+    }
+}
