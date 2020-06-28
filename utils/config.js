4f43be0d0052e29d9ed7ed891eda14fde3182ef0
Thuvarahan <47806859+thuvarahan97@users.noreply.github.com>
2020-06-28 13:17:34 +0530

    Updated config.js for heroku server

--- utils/config.js
+++ utils/config.js
@@ -1,19 +1,19 @@
 //Local Server Database
-exports.database = {
-    host: 'localhost',
-    user: 'root',
-    name: 'sieve',
-    password: '',
-    port: '3306',
-    multipleStatements: true
-};
-
-//Heroku Server Database
 // exports.database = {
-//     host: 'eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
-//     user: 'xnvxi3kclh1ehyiz',
-//     name: 'zegsirx7c1nzizrd',
-//     password: 'dqxqu2yrbctijlod',
+//     host: 'localhost',
+//     user: 'root',
+//     name: 'sieve',
+//     password: '',
 //     port: '3306',
 //     multipleStatements: true
 // };
+
+//Heroku Server Database
+exports.database = {
+    host: 'eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+    user: 'xnvxi3kclh1ehyiz',
+    name: 'zegsirx7c1nzizrd',
+    password: 'dqxqu2yrbctijlod',
+    port: '3306',
+    multipleStatements: true
+};
